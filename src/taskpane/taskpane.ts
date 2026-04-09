5ef230ce88526f950bc023a5e6cf52e44d21c809
kwtSMS Admin <support@kwtsms.com>
2026-04-09 16:16:29 +0300

    Bump to v1.0.19, update SupportUrl to kwtsms.com

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -13,7 +13,7 @@ import { ensureLogSheet, logBatch } from "../services/logger";
 import { getString, formatString } from "../localization/strings";
 import { LogEntry, SendStatus, SendResponse } from "../models/types";
 
-const APP_VERSION = "1.0.18";
+const APP_VERSION = "1.0.19";
 const BATCH_SIZE = 200;
 const BATCH_DELAY_MS = 200;
 
